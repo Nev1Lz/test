25b27ea124f299e6a1c6df19a891a0683d29c684
Vlad <vlad.makeevv@yandex.ru>
2018-12-03 09:58:57 +0300

    Это наш первмой первый коммит

---
+++ script.js
@@ -0,0 +1,46 @@
+'use strict';
+
+let a = 5;
+console.log(a);
+
+// console.log(4/0);
+// console.log('string'*9);
+
+// let something;
+// console.log(something);
+
+// let person = {
+//     name: "Vlad",
+//     age: 22,
+//     isMarried: false
+// }
+
+// console.log(person["name"]);
+
+// let arr = ['plum', 'png', 'jpeg'];
+
+// console.log(arr[2]);
+
+// alert("Hello World!");
+
+// let answer = confirm("Are you here?");
+// console.log(answer);
+
+// let answer = +prompt("Есть ли вам 18?", "Да");
+// console.log(answer);
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(++incr);
+console.log(decr--);
+
+console.log(6%2);
+console.log("2" === 2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || isClose);
+
